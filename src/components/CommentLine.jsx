b21e76323b3543377c9f6a4f3c77477c7c91c459
jjaybah <d.jaylah@gmail.com>
2024-12-17 17:40:44 +0000

    display username of logged in user as 'You'

--- src/components/CommentLine.jsx
+++ src/components/CommentLine.jsx
@@ -1,9 +1,16 @@
 import moment from "moment";
+import { useContext } from "react";
+import { UserContext } from "../contexts/userContext";
+import { useAuthorDisplayName } from "../utils/utils";
+
 const CommentLine = ({ comment }) => {
+  const { user } = useContext(UserContext);
+  const author = useAuthorDisplayName(comment.author, user);
+
   return (
     <div className="comment__card">
       <div className="comment__header">
-        <span className="comment__author">{comment.author}</span>
+        <span className="comment__author">{author}</span>
         <span className="comment__created">
           {moment(comment.created_at).format("DD MMM, YYYY HH:mm")}
         </span>
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1>NC News Home Page</h1>;
+};
+
+export default HomePage;
