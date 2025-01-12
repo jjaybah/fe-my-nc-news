ba9337b9ff5310c1ec3e22d35012dce75df4e81b
jjaybah <d.jaylah@gmail.com>
2025-01-12 21:34:40 +0000

    add home page content

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,14 @@
 const HomePage = () => {
   return (
-    <>
-      <h1>NC News Home Page</h1>
-      <p>Not here yet. But will be one day</p>
-    </>
+    <section className="home__page__content">
+      <h1>Welcome to Northcoders News!</h1>
+      <p>
+        {`To start exploring, please click on "All
+        News" in the navigation bar.`}
+        <p>The database may take a moment to load on your first visit⏳</p>
+      </p>
+      <p>Thank you for your patience!</p>
+    </section>
   );
 };
 
